5b0ba69ed47b262dc41942a0f49b6c936f5b6a4c
aarongough <aaron@aarongough.com>
2010-10-02 18:18:56 -0400

    Added tests and code for implicitly positive and negative integers and floats.

---
+++ test/unit/float_literal_test.rb
@@ -0,0 +1,30 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_helper.rb'))
+
+class FloatLiteralTest < Test::Unit::TestCase
+
+  def setup
+    @parser = Sexpistol.new
+  end
+
+  test "should parse sexp containing an implicitly positive float literal" do
+    ast = @parser.parse_string("10.00")
+    assert_equal [10.00], ast
+  end
+  
+  test "should parse sexp containing an explicitly positive float literal" do
+    ast = @parser.parse_string("+910.00")
+    assert_equal [910.00], ast
+  end
+  
+  test "should parse sexp containing an explicitly negative float literal" do
+    ast = @parser.parse_string("-10.00")
+    assert_equal [-10.00], ast
+  end
+  
+  test "should parse sexp containing a large float literal" do
+    ast = @parser.parse_string("1.0000127829")
+    assert_equal [1.0000127829], ast
+  end
+    
+    
+end
--- test/unit/integer_literal_test.rb
+++ test/unit/integer_literal_test.rb
@@ -2,8 +2,23 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_helper.rb
 
 class IntegerLiteralTest < Test::Unit::TestCase
 
-  test "should parse sexp containing integer literal" do
+  def setup
+    @parser = Sexpistol.new
+  end
+
+  test "should parse sexp containing an implicitly positive integer literal" do
+    ast = @parser.parse_string("10")
+    assert_equal [10], ast
+  end
   
+  test "should parse sexp containing an explicitly positive integer literal" do
+    ast = @parser.parse_string("+910")
+    assert_equal [910], ast
+  end
+  
+  test "should parse sexp containing an explicitly negative integer literal" do
+    ast = @parser.parse_string("-10")
+    assert_equal [-10], ast
   end
     
 end
