9dca4d1a4f150e8a31cca7d86ab20412f189dde4
aarongough <aaron@aarongough.com>
2010-10-06 14:53:47 -0400

    Added simple performance test.

---
+++ test/performance/benchmark_test.rb
@@ -0,0 +1,34 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_helper.rb'))
+
+class BenchmarkTest < Test::Unit::TestCase
+
+  require 'benchmark'
+
+  test "benchmark sexpistol" do
+    puts "\nRunning performance test...\n"
+    Benchmark.bmbm do |b|
+      b.report do
+        5000.times do
+          parser = Sexpistol.new
+          parser.ruby_keyword_literals = true
+          parser.parse_string <<-EOD
+          
+            (display "This is a test string!")
+            
+            (define test (lambda () (begin
+              (display (== 1 1))
+              (display (== true true))
+              (display (== false false))
+              (display (== nil nil))
+              (display (== 2.09 1.08))
+              (display (== 2e6 2e12))
+            )))
+            
+          EOD
+        end
+      end
+    end
+    puts
+  end    
+    
+end
